1fd62e2700786c8498372613a917206e4d077b8d
Jack Mead <jack-mead@hotmail.co.uk>
2022-08-23 21:12:38 +0100

    Adjust blob storage to be publicly accessible

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -65,5 +65,5 @@ resource "azurerm_storage_account" "main" {
 resource "azurerm_storage_container" "main" {
   name                  = "images"
   storage_account_name  = azurerm_storage_account.main.name
-  container_access_type = "private"
+  container_access_type = "blob"
 }
